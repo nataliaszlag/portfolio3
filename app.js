82c4077f7346f22be007f94e6d7fecdb6cb3a589
Adam S <a.s.szlag@gmail.com>
2020-10-11 20:28:26 +0200

    almost done

--- app.js
+++ app.js
@@ -39,3 +39,82 @@ const imgs = [
       }
     }
   }
+
+
+  //////////arrow///////////
+
+  function currentId1() {
+        document.getElementById("circle-home").classList.add("current");
+        document.getElementById("circle-about").classList.remove("current");
+        document.getElementById("circle-portfolio").classList.remove("current");
+        document.getElementById("circle-gallery").classList.remove("current");
+        document.getElementById("circle-contact").classList.remove("current");
+
+  }
+
+  function currentId2() {
+    document.getElementById("circle-home").classList.remove("current");
+    document.getElementById("circle-about").classList.add("current");
+    document.getElementById("circle-portfolio").classList.remove("current");
+    document.getElementById("circle-gallery").classList.remove("current");
+    document.getElementById("circle-contact").classList.remove("current");
+
+}
+
+  function currentId3() {
+    document.getElementById("circle-home").classList.remove("current");
+    document.getElementById("circle-about").classList.remove("current");
+    document.getElementById("circle-portfolio").classList.add("current");
+    document.getElementById("circle-gallery").classList.remove("current");
+    document.getElementById("circle-contact").classList.remove("current");
+
+  }
+
+function currentId4() {
+  document.getElementById("circle-home").classList.remove("current");
+  document.getElementById("circle-about").classList.remove("current");
+  document.getElementById("circle-portfolio").classList.remove("current");
+  document.getElementById("circle-gallery").classList.add("current");
+  document.getElementById("circle-contact").classList.remove("current");
+
+}
+
+function currentId5() {
+  document.getElementById("circle-home").classList.remove("current");
+  document.getElementById("circle-about").classList.remove("current");
+  document.getElementById("circle-portfolio").classList.remove("current");
+  document.getElementById("circle-gallery").classList.remove("current");
+  document.getElementById("circle-contact").classList.add("current");
+
+}
+
+  let idCurrent1 = document.getElementById("home");
+  let idCurrent2 = document.getElementById("about");
+  let idCurrent3 = document.getElementById("portfolio");
+  let idCurrent4 = document.getElementById("gallery");
+  let idCurrent5 = document.getElementById("contact");
+
+  let homeHeight = idCurrent1.offsetHeight;
+  let aboutHeight = idCurrent2.offsetHeight;
+  let portfolioHeight = idCurrent3.offsetHeight;
+  let galleryHeight = idCurrent4.offsetHeight;
+
+
+window.addEventListener("scroll", ()=> {
+
+    if(window.pageYOffset <= homeHeight){
+    currentId1();
+    }
+    else if(window.pageYOffset > homeHeight && window.pageYOffset <= (aboutHeight+homeHeight)){
+      currentId2();
+    }
+    else if(window.pageYOffset > (aboutHeight+homeHeight)&& window.pageYOffset <= (aboutHeight+homeHeight+portfolioHeight)){
+      currentId3();
+    }
+    else if(window.pageYOffset > (portfolioHeight+homeHeight+aboutHeight)&& window.pageYOffset <= (aboutHeight+homeHeight+portfolioHeight+galleryHeight)){
+      currentId4();
+    }
+    else if(window.pageYOffset > (portfolioHeight+homeHeight+aboutHeight+galleryHeight)){
+      currentId5();
+    }
+})
